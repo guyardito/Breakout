ad58dc1f741c0039fef57fea7ea8a6a784e90feb
guyardito <guyardito@gmail.com>
2023-01-21 15:07:49 -0500

    Initial Commit

---
+++ Breakout/BreakoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakoutApp.swift
+//  Breakout
+//
+//  Created by Dev on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
